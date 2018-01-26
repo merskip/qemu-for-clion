30f190988d7958fbdce0178f9f8180358b7e5dbb
Piotr Merski <merskip@gmail.com>
2018-01-26 18:15:14 +0100

    Debugging OS running on QEMU with GDB remote

--- src/pl/merskip/qemu/QemuLauncher.java
+++ src/pl/merskip/qemu/QemuLauncher.java
@@ -3,19 +3,21 @@ package pl.merskip.qemu;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.filters.TextConsoleBuilder;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
-import com.intellij.execution.process.BaseOSProcessHandler;
-import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import com.intellij.xdebugger.XDebugSession;
-import com.jetbrains.cidr.cpp.execution.CLionRunParameters;
 import com.jetbrains.cidr.cpp.execution.debugger.backend.GDBDriverConfiguration;
 import com.jetbrains.cidr.cpp.toolchains.CPPToolchains;
-import com.jetbrains.cidr.execution.TrivialInstaller;
 import com.jetbrains.cidr.execution.debugger.CidrDebugProcess;
-import com.jetbrains.cidr.execution.debugger.CidrLocalDebugProcess;
+import com.jetbrains.cidr.execution.debugger.remote.CidrRemoteDebugParameters;
+import com.jetbrains.cidr.execution.debugger.remote.CidrRemoteGDBDebugProcess;
 import com.jetbrains.cidr.execution.testing.CidrLauncher;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
 
 public class QemuLauncher extends CidrLauncher {
 
@@ -27,25 +29,40 @@ public class QemuLauncher extends CidrLauncher {
 
     @Override
     protected ProcessHandler createProcess(@NotNull CommandLineState commandLineState) throws ExecutionException {
-        GeneralCommandLine commandLine = createGeneralCommandLine();
+        GeneralCommandLine commandLine = createQemuCommandLine();
         return new BaseOSProcessHandler(commandLine.createProcess(), commandLine.getPreparedCommandLine(), commandLine.getCharset());
     }
 
     @NotNull
     @Override
     protected CidrDebugProcess createDebugProcess(@NotNull CommandLineState commandLineState, @NotNull XDebugSession xDebugSession) throws ExecutionException {
-        GeneralCommandLine commandLine = createGeneralCommandLine();
+        Process qemuProcess = createQemuCommandLine().createProcess();
+        CidrRemoteGDBDebugProcess remoteGDBProcess = createRemoteGDBProcess("tcp:localhost:1234",
+                "/home/merskip/Workspace/PiomekOS/cmake-build-debug/kernel.bin", xDebugSession);
 
+        remoteGDBProcess.getProcessHandler().addProcessListener(new ProcessAdapter() {
+
+            @Override
+            public void processTerminated(@NotNull ProcessEvent event) {
+                qemuProcess.destroy();
+            }
+        });
+
+        return remoteGDBProcess;
+    }
+
+    private CidrRemoteGDBDebugProcess createRemoteGDBProcess(String remoteCommand, String symbolFile, XDebugSession xDebugSession) throws ExecutionException {
         CPPToolchains.Toolchain toolchain = CPPToolchains.getInstance().getDefaultToolchain();
         if (toolchain == null) {
             throw new IllegalStateException("No default toolchain");
         }
-        GDBDriverConfiguration debuggerConfiguration = new GDBDriverConfiguration(getProject(), toolchain);
-        CLionRunParameters runParameters = new CLionRunParameters(debuggerConfiguration, new TrivialInstaller(commandLine));
-        return new CidrLocalDebugProcess(runParameters, xDebugSession, TextConsoleBuilderFactory.getInstance().createBuilder(getProject()));
+        GDBDriverConfiguration debuggerConfiguration = new GDBDriverConfiguration(project, toolchain);
+        CidrRemoteDebugParameters debugParameters = new CidrRemoteDebugParameters(remoteCommand, symbolFile,"", Collections.emptyList());
+        TextConsoleBuilder textConsoleBuilder = TextConsoleBuilderFactory.getInstance().createBuilder(project);
+        return new CidrRemoteGDBDebugProcess(debuggerConfiguration, debugParameters, xDebugSession, textConsoleBuilder);
     }
 
-    private GeneralCommandLine createGeneralCommandLine() {
+    private GeneralCommandLine createQemuCommandLine() {
         return new GeneralCommandLine("qemu-system-x86_64", "-s", "-S", "-cdrom", "/home/merskip/Workspace/PiomekOS/cmake-build-debug/PiomekOS.iso");
     }
 
